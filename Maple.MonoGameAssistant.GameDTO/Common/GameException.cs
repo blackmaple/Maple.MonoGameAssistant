07225da926d1f8ffde2067113755a30abfed8f84
红色枫叶 <BlackMaple@live.cn>
2024-07-30 22:13:53 +0800

    1.add mono data

--- Maple.MonoGameAssistant.GameDTO/Common/GameException.cs
+++ Maple.MonoGameAssistant.GameDTO/Common/GameException.cs
@@ -12,5 +12,12 @@ namespace Maple.MonoGameAssistant.GameDTO
         [DoesNotReturn]
         public static T Throw<T>(string msg) => throw new GameException(msg);
 
+
+        [DoesNotReturn]
+        public static T ThrowIfNotLoaded<T>() => throw new GameException("Please load the game save first");
+
+        [DoesNotReturn]
+        public static void ThrowIfNotLoaded(string msg) => throw new GameException("Please load the game save first");
+
     }
 }
