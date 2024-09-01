edabc18bc54f6302e528cf9ac3b2eafc571c8f32
红色枫叶 <BlackMaple@live.cn>
2024-09-01 10:58:13 +0800

    1

--- Maple.MonoGameAssistant.Core/MonoService/MonoRuntimeFactory.cs
+++ Maple.MonoGameAssistant.Core/MonoService/MonoRuntimeFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 namespace Maple.MonoGameAssistant.Core
 {
@@ -13,6 +14,18 @@ namespace Maple.MonoGameAssistant.Core
         [MethodImpl(MethodImplOptions.Synchronized)]
         public bool CreateMonoRuntime(out EnumMonoRuntimeType runtimeType)
         {
+            return this.TryCreateMonoRuntime(out _, out runtimeType);
+        }
+
+        public IMonoRuntiemProvider GetProvider()
+        {
+            return this.MonoRuntimeService
+                ?? MonoRuntimeException.Throw<IMonoRuntiemProvider>($"Mono Runtime Type is {this.RuntimeApi.RuntimeType} Not Loaded");
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public bool TryCreateMonoRuntime([MaybeNullWhen(false)] out IMonoRuntiemProvider monoRuntiemProvider, out EnumMonoRuntimeType runtimeType)
+        {
             var init = this.RuntimeApi.TryLoadMonoRuntimeApi();
             if (init)
             {
@@ -23,37 +36,31 @@ namespace Maple.MonoGameAssistant.Core
                     _ => GetMonoRuntime_ERROR(),
                 };
             }
+            monoRuntiemProvider = this.MonoRuntimeService;
             runtimeType = this.RuntimeApi.RuntimeType;
             return init;
 
-            IMonoRuntiemProvider GetMonoRuntime_MONO()
-            {
-                ILogger logger = loggerFactory.CreateLogger<MonoRuntiemProvider_MONO>();
-                return new MonoRuntiemProvider_MONO(logger, this.RuntimeApi);
-            }
-
-            IMonoRuntiemProvider GetMonoRuntime_IL2CPP()
-            {
-                ILogger logger = loggerFactory.CreateLogger<MonoRuntiemProvider_IL2CPP>();
-                return new MonoRuntiemProvider_IL2CPP(logger, this.RuntimeApi);
-            }
-
-            IMonoRuntiemProvider GetMonoRuntime_ERROR()
-            {
-                return MonoRuntimeException.Throw<IMonoRuntiemProvider>($"Mono Runtime Type is {this.RuntimeApi.RuntimeType} Error");
-            }
 
 
 
         }
 
-        public IMonoRuntiemProvider GetProvider()
+        MonoRuntiemProvider_MONO GetMonoRuntime_MONO()
         {
-            return this.MonoRuntimeService
-                ?? MonoRuntimeException.Throw<IMonoRuntiemProvider>($"Mono Runtime Type is {this.RuntimeApi.RuntimeType} Not Loaded");
+            ILogger logger = loggerFactory.CreateLogger<MonoRuntiemProvider_MONO>();
+            return new MonoRuntiemProvider_MONO(logger, this.RuntimeApi);
         }
 
+        MonoRuntiemProvider_IL2CPP GetMonoRuntime_IL2CPP()
+        {
+            ILogger logger = loggerFactory.CreateLogger<MonoRuntiemProvider_IL2CPP>();
+            return new MonoRuntiemProvider_IL2CPP(logger, this.RuntimeApi);
+        }
 
+        IMonoRuntiemProvider GetMonoRuntime_ERROR()
+        {
+            return MonoRuntimeException.Throw<IMonoRuntiemProvider>($"Mono Runtime Type is {this.RuntimeApi.RuntimeType} Error");
+        }
 
 
     }
