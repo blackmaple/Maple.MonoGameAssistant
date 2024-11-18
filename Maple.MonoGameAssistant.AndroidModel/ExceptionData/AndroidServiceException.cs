4ee652b795d9029ecf102ca2328e4063737acb3f
红色枫叶 <BlackMaple@live.cn>
2024-11-18 08:06:41 +0800

    1.add android files

--- Maple.MonoGameAssistant.AndroidModel/ExceptionData/AndroidServiceException.cs
+++ Maple.MonoGameAssistant.AndroidModel/ExceptionData/AndroidServiceException.cs
@@ -3,18 +3,14 @@ using System.Runtime.CompilerServices;
 
 namespace Maple.MonoGameAssistant.AndroidModel.ExceptionData
 {
-    public sealed class AndroidJNIException([CallerMemberName] string msg = nameof(AndroidJNIException))
-        : AndroidCommonException(msg)
+    public sealed class AndroidServiceException([CallerMemberName] string msg = nameof(AndroidJNIException)) : AndroidCommonException(msg)
     {
-
-
         [DoesNotReturn]
         public static void Throw([CallerMemberName] string msg = nameof(AndroidJNIException))
-            => throw new AndroidJNIException(msg);
+        => throw new AndroidServiceException(msg);
 
         [DoesNotReturn]
         public static T Throw<T>([CallerMemberName] string msg = nameof(AndroidJNIException))
-            => throw new AndroidJNIException(msg);
-
+            => throw new AndroidServiceException(msg);
     }
 }
