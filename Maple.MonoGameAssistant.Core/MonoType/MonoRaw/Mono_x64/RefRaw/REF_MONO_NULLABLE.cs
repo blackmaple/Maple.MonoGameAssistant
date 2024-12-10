6a3cde2ee02e4f02ca0ee71cfb566f314e1aec6e
红色枫叶 <BlackMaple@live.cn>
2024-12-10 16:44:56 +0800

    1.modify data

--- Maple.MonoGameAssistant.Core/MonoType/MonoRaw/Mono_x64/RefRaw/REF_MONO_NULLABLE.cs
+++ Maple.MonoGameAssistant.Core/MonoType/MonoRaw/Mono_x64/RefRaw/REF_MONO_NULLABLE.cs
@@ -6,11 +6,17 @@ namespace Maple.MonoGameAssistant.Core
     public readonly unsafe partial struct REF_MONO_NULLABLE<T>
         where T : unmanaged
     {
+        public REF_MONO_NULLABLE(T? val)
+        {
+            if (val.HasValue)
+            {
+                hasValue = true;
+                value = val.Value;
+            }
+        }
 
         [MarshalAs(UnmanagedType.U1)]
         public readonly System.Boolean hasValue;
-
-
 
         public readonly T value;
 
